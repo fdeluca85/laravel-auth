59f5104eea7ae234671ba2d3ab974ff1689bf00a
francesca <fradeluca85@gmail.com>
2022-02-15 19:10:36 +0100

    index.blade

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -18,7 +18,7 @@ class PostsTableSeeder extends Seeder
             $newPost = new Post();
             $newPost->title = $faker->words(7, true);
             $newPost->slug = Str::of($newPost->title)->slug("-");
-            $newPost->content = $faker->text();
+            $newPost->content = $faker->text(300);
             $newPost->published = rand(0,1);
             $newPost->save();
         }
---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">Posts List</div>
+
+                <div class="card-body">
+                    @foreach ($posts as $post)
+                    <h2>{{$post->title}}</h2>
+                    <p>{{$post->content}}</p>
+                        
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
