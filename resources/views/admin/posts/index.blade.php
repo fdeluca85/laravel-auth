d7d95ba372a7443ed14c044afd50e2a7a3b8fa31
francesca <fradeluca85@gmail.com>
2022-02-16 16:14:21 +0100

    index & show

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -8,11 +8,30 @@
                 <div class="card-header">Posts List</div>
 
                 <div class="card-body">
-                    @foreach ($posts as $post)
-                    <h2>{{$post->title}}</h2>
-                    <p>{{$post->content}}</p>
-                        
-                    @endforeach
+                    <table class="table table-striped">
+                        <thead>
+                          <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Titolo</th>
+                            <th scope="col">Slug</th>
+                            <th scope="col">Visualizza</th>
+                            <th scope="col">Modifica</th>
+                            <th scope="col">Elimina</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                        @foreach ($posts as $post)
+                          <tr>
+                            <th scope="row">{{$post->id}}</th>
+                            <td>{{$post->title}}</td>
+                            <td>{{$post->slug}}</td>
+                            <td><a href="{{route("posts.show",$post->id)}}"><button type="button" class="btn btn-primary">Vai</button></a></td>
+                            
+                          </tr>
+                          @endforeach
+
+                        </tbody>
+                      </table>
                 </div>
             </div>
         </div>
